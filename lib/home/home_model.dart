12d83d92c19ab68fbeb3ab4a99de9f45f04625b0
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-09-20 11:07:22 +0300

    removed useless code

--- lib/home/home_model.dart
+++ lib/home/home_model.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_translate/flutter_translate.dart';
 import 'package:turskyi/home/home_view.dart';
 
 class HomeModel with ChangeNotifier {
-  HomeView _view;
+  HomeView _view; // ignore: unused_field
   bool _isLoading = false;
 
   TextEditingController phoneEditingController = TextEditingController();
@@ -11,18 +10,4 @@ class HomeModel with ChangeNotifier {
   bool get isLoading => _isLoading;
 
   HomeModel(this._view);
-
-  void login() async {
-    _isLoading = true;
-    notifyListeners();
-
-    try {
-
-    } catch (e) {
-      print(e);
-      _view.displayMessage(translate("home.error"));
-    }
-    _isLoading = false;
-    notifyListeners();
-  }
 }
