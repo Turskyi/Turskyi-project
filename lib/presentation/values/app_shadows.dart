cef081df5767da264eb332536f914aa2f04ca474
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2022-11-13 18:00:39 -0500

    Dev (#6)
    * Update comments in main.dart
    
    * Update actions/checkout@v2 and subosito/flutter-action@v1 to use Node.js 16.
    
    * deploy after push to dev branch instead of master
    
    * fix CI
    
    * set deployment to master branch of https://github.com/Turskyi/turskyi.github.io
    
    * delete duplicated line of code
    
    * correct documentation

--- lib/presentation/values/app_shadows.dart
+++ lib/presentation/values/app_shadows.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppShadows {
-  /// [topRightTextShadow] [Shadow] variable for "shadows" parameter
+  /// [Shadow] variable for "shadows" parameter
   /// of [TextStyle], to give a shadow to a [Text]
   static const Shadow topRightTextShadow = Shadow(
     // topRight
     offset: Offset(1.0, 1.0), blurRadius: 4.0,
   );
 
-  /// [bottomLeftTextShadow] [Shadow] variable for "shadows" parameter
+  /// [Shadow] variable for "shadows" parameter
   /// of [TextStyle], to give a shadow to a [Text]
   static const Shadow bottomLeftTextShadow = Shadow(
     // bottomLeft
