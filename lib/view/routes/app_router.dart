2ea392006e1803e99819bbbde705e12e5ba27615
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-06-15 21:22:30 -0400

    refactor project structure to follow "Monolithic Model-View-Presenter" pattern.

--- lib/view/routes/app_router.dart
+++ lib/view/routes/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:turskyi/view/pages/game/game_page.dart';
-import 'package:turskyi/view/pages/game/unity_game_page.dart';
 import 'package:turskyi/view/pages/home/home_page.dart';
 import 'package:turskyi/view/routes/app_route.dart';
 
@@ -10,7 +9,6 @@ class AppRouter {
     return <String, WidgetBuilder>{
       AppRoute.home.path: (_) => const HomePage(),
       AppRoute.game.path: (_) => const GamePage(),
-      AppRoute.unityGame.path: (_) => const UnityGamePage(),
     };
   }
 }
