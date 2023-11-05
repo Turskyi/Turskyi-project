61d684999e4685571c852713f8dc81acdf2d5873
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-11-05 16:58:58 -0500

    Add unity 2d game

--- lib/main/res/values/strings.dart
+++ lib/main/res/values/strings.dart
@@ -6,4 +6,11 @@ class Strings {
   static const String phoneNumber = '+14379852581';
   static const String oneHundredPercent = '100%';
   static const String noBorder = 'none';
+  static const String unityLogo = 'unity_logo.svg';
+
+  static const String unity2dLogo = 'unity_2d_logo.png';
+
+  static const String googlePlayLogo = 'pic_google_play_grey.png';
+
+  static const String homeBackground = 'bg_home.png';
 }
