de98df51278bc974cc8dab29c3e29e03ed039e6e
Dmytro Turskyi <dmytro@turskyi.com>
2024-08-08 18:50:25 -0400

    fix tests
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

---
+++ test/screen_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:turskyi/view/util/screen.dart';
+
+void main() {
+  testWidgets('isWide returns true for wide screens',
+      (WidgetTester tester) async {
+    // Define a wide screen size
+    const Size wideScreenSize = Size(1200, 800);
+
+    // Build a widget with the specified screen size
+    await tester.pumpWidget(
+      MediaQuery(
+        data: const MediaQueryData(size: wideScreenSize),
+        child: Builder(
+          builder: (BuildContext context) {
+            // Verify that isWide returns true for wide screens
+            expect(isWide(context), isTrue);
+            return Container();
+          },
+        ),
+      ),
+    );
+  });
+
+  testWidgets('isWide returns false for narrow screens',
+      (WidgetTester tester) async {
+    // Define a narrow screen size
+    const Size narrowScreenSize = Size(599, 800);
+
+    // Build a widget with the specified screen size
+    await tester.pumpWidget(
+      MediaQuery(
+        data: const MediaQueryData(size: narrowScreenSize),
+        child: Builder(
+          builder: (BuildContext context) {
+            // Verify that isWide returns false for narrow screens
+            expect(isWide(context), isFalse);
+            return Container();
+          },
+        ),
+      ),
+    );
+  });
+
+  testWidgets('isWide returns true for screens just above the threshold',
+      (WidgetTester tester) async {
+    // Define a screen size just above the threshold
+    const Size justAboveThresholdSize = Size(600, 800);
+
+    // Build a widget with the specified screen size
+    await tester.pumpWidget(
+      MediaQuery(
+        data: const MediaQueryData(size: justAboveThresholdSize),
+        child: Builder(
+          builder: (BuildContext context) {
+            // Debug print
+            print(
+              'Screen size just above threshold: ${MediaQuery.of(context).size.shortestSide}',
+            );
+            // Verify that isWide returns true for screens just above the threshold
+            expect(isWide(context), isTrue);
+            return Container();
+          },
+        ),
+      ),
+    );
+  });
+}
